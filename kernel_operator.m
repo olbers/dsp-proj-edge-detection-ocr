e869de20ffc7ae429916220fafa58ac82d2a969d
ernest.carozza <ernest.carozza@gmail.com>
2010-12-05 05:15:05 +0000

    Added Scharr operator to GUI

--- kernel_operator.m
+++ kernel_operator.m
@@ -2,7 +2,8 @@
 % Input: 
 %   -the image: an m by n [by d] matrix. If it is RBG (ie d=3) it will
 %   be converted to grayscale (m by n)
-%   -the operator type: either 'Prewitt' or 'Sobel' or 'Roberts Cross'
+%   -the operator type: either 'Prewitt' or 'Sobel' or 'Roberts Cross' or
+%   'Scharr'
 % Output:
 %   -m by n matrix of magnitudes of gradients computed for each pixel
 %   -m by n matrix of directions of gradients computer for each pixel
@@ -25,9 +26,13 @@ elseif strcmp(operator_type, 'Sobel')
 elseif strcmp(operator_type, 'Roberts Cross')
     Kx = [[1 0];[0 -1]];
     Ky = [[0 1];[-1 0]];
-elseif strcmp(operator_type, 'Scharr Operator')
-    Kx = [[3 10 3]; [0 0 0]; [-3, -10, -3]];
-    Ky = [[3 0 3]; [10 0 -10]; [3 0 -3]];
+elseif strcmp(operator_type, 'Scharr')
+    % Note: Added constant 1/3 to sort of normalize: because of the
+    % difference in values compared to the other kernel operators, the 
+    % magnitude of this gradient will be much larger . This is a problem 
+    % for when adjusting the threshold in the GUI
+    Kx = 1/3*[[3 10 3]; [0 0 0]; [-3, -10, -3]];
+    Ky = 1/3*[[3 0 3]; [10 0 -10]; [3 0 -3]];
 else
     errordlg(sprintf('Error: %s is not a valid type', operator_type));
     edge_image = 0;

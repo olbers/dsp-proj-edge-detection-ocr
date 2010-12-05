63215c7e725d3a11f59a89862e2627e57ed0fa29
ernest.carozza <ernest.carozza@gmail.com>
2010-12-05 04:45:06 +0000

    Integrated crispening to the GUI

--- crisp.m
+++ crisp.m
@@ -1,26 +1,28 @@
 % "Crispens" the image by adding the edges to the original image
 % Note that the crispened image will be a grayscale image
 % Input: 
-%   -the image: an m by n [by d] matrix. If it is RBG (ie d=3) it will
+%   -the source image: an m by n [by d] matrix. If it is RBG (ie d=3) it will
 %   be converted to grayscale (m by n)
+%   -the matrix of edges, either binary or grayscale
 %   -the "crispening" factor - the degree by which the image will appear to
 %   be sharpened
 % Output:
 %   -m by n matrix of enhanced edges computed for each pixel
 
 
-function [crisp_image] = crisp(edge_image,a)
+function [crisp_image] = crisp(source_image, edge_image, a)
 
 if length(size(edge_image)) == 3
-    t_image = rgb2gray(edge_image);
+    source_image = rgb2gray(source_image);
 end
 
-[h,w] = size (edge_image);
-crisp_image = zeros(h,w);
+crisp_image = source_image + a * 1 * edge_image;
 
-new_image = kernel_operator(t_image, operator_type);
-for i= 1:h
-    for j = 1:w
-        crisp_image(i,j) =(t_image(i,j)+a*0.1*new_image(i,j));
-    end
-end
+%[h,w] = size (edge_image);
+%crisp_image = zeros(h,w);
+
+%for i= 1:h
+%    for j = 1:w
+%        crisp_image(i,j) =(t_image(i,j)+a*0.1*new_image(i,j));
+%    end
+%end

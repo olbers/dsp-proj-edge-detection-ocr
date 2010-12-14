511ab654aef999bc1c7f903bbcf465528e11f113
ernest.carozza <ernest.carozza@gmail.com>
2010-12-14 08:02:29 +0000

    Massive changes to ocr

--- preprocess_image.m
+++ preprocess_image.m
@@ -1,15 +1,27 @@
 % Given an image, finds the skeleton and returns it in a 32x32 binary image
-function [newim] = preprocess_image(source_im)
-clc;
+function [newim skeleton] = preprocess_image(source_im)
+crop_size = [20 20];
 
-crop_size = [32 32];
+% Try to get rid of some serifs -- kill pixels with only 1 white neighbor
+[m,n] = size(source_im);
+mark_for_delete = zeros(m,n);
+for i=1:m
+    for j=1:n
+        if source_im(i,j) == 1
+            if sum(sum(source_im(i-1:i+1,j-1:j+1))) == 2
+                mark_for_delete(i,j) = 1.0;
+            end
+        end
+    end
+end
+source_im = source_im - mark_for_delete;
 
 
 % Strategy: find extrema in each direction, crop, then resize
-resized_im = imresize(crop(source_im),crop_size,'nearest');
+newim = imresize(crop(source_im),crop_size,'nearest');
 
 % Now thin the image out (ie find the skeleton)
-newim = skel(resized_im);
+skeleton = skel(newim);
 
     function cropped_im = crop(im)
         [m,n] = size(im);
@@ -17,19 +29,19 @@ newim = skel(resized_im);
         last_i = m;
         first_j = n;
         last_j = n;
-        for k=1:m
-            if sum(im(k,:)) > 0.0
-                first_i = min(first_i, k);
-                last_i = k;
+        for kk=1:m
+            if sum(im(kk,:)) > 0.0
+                first_i = min(first_i, kk);
+                last_i = kk;
             end
         end
-        for j=1:n
-            if sum(im(:,j)) > 0.0
-                first_j = min(first_j, j);
-                last_j = j;
+        for jj=1:n
+            if sum(im(:,jj)) > 0.0
+                first_j = min(first_j, jj);
+                last_j = jj;
             end
         end
         
-        cropped_im = im(first_i:last_i,first_j:last_j);
+        cropped_im = im(first_i-1:last_i+1,first_j-1:last_j+1);
     end
 end
